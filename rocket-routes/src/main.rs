a5342cb0f82bcfc19536e7e5d8d63f38dcfd949e
Fabio William Conceição <messhias@gmail.com>
2023-01-22 12:17:18 +0000

    finished routes and resquest study with rocket.

--- rocket-routes/src/main.rs
+++ rocket-routes/src/main.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket, request::FromParam};
-use std::collections::HashMap;
 use lazy_static::lazy_static;
-use rocket::http::ContentType;
+use rocket::http::{ContentType, Status};
+use rocket::request::{FromParam, Request};
 use rocket::response::{self, Responder, Response};
+use rocket::{Build, Rocket};
+use std::collections::HashMap;
 use std::io::Cursor;
-use rocket::response::status;
+use std::vec::Vec;
 
 #[derive(FromForm)]
 struct Filters {
@@ -22,17 +23,17 @@ struct NameGrade<'r> {
 
 impl<'r> FromParam<'r> for NameGrade<'r> {
     type Error = &'static str;
+
     fn from_param(param: &'r str) -> Result<Self, Self::Error> {
         const ERROR_MESSAGE: Result<NameGrade, &'static str> = Err("Error parsing user parameter");
-        let name_grade_vec: Vec<&'r str> = param.split("_").collect();
+
+        let name_grade_vec: Vec<&'r str> = param.split('_').collect();
         match name_grade_vec.len() {
             2 => match name_grade_vec[1].parse::<u8>() {
-                Ok(n) => Ok(
-                    Self {
-                        name: name_grade_vec[0],
-                        grade: n,
-                    }
-                ),
+                Ok(n) => Ok(Self {
+                    name: name_grade_vec[0],
+                    grade: n,
+                }),
                 Err(_) => ERROR_MESSAGE,
             },
             _ => ERROR_MESSAGE,
@@ -49,9 +50,37 @@ struct User {
     active: bool,
 }
 
+impl<'r> Responder<'r, 'r> for &'r User {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'r> {
+        let user = format!("Found user: {:?}", self);
+        Response::build()
+            .sized_body(user.len(), Cursor::new(user))
+            .raw_header("X-USER-ID", self.uuid.to_string())
+            .header(ContentType::Plain)
+            .ok()
+    }
+}
+
+struct NewUser<'a>(Vec<&'a User>);
+
+impl<'r> Responder<'r, 'r> for NewUser<'r> {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'r> {
+        let user = self
+            .0
+            .iter()
+            .map(|u| format!("{:?}", u))
+            .collect::<Vec<String>>()
+            .join(",");
+        Response::build()
+            .sized_body(user.len(), Cursor::new(user))
+            .header(ContentType::Plain)
+            .ok()
+    }
+}
+
 lazy_static! {
     static ref USERS: HashMap<&'static str, User> = {
-    let mut map = HashMap::new();
+        let mut map = HashMap::new();
         map.insert(
             "3e3dd4ae-3c37-40c6-aa64-7061f284ce28",
             User {
@@ -62,49 +91,40 @@ lazy_static! {
                 active: true,
             },
         );
+        map.insert(
+            "abc123",
+            User {
+                uuid: String::from("acb123"),
+                name: String::from("Fabio"),
+                age: 33,
+                grade: 2,
+                active: true,
+            },
+        );
         map
     };
 }
 
-impl<'r> Responder<'r, 'r> for &'r User {
-    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'r> {
-        let base_response = default_response();
-        let user = format!("Found user: {:?}", self);
-        Response::build()
-            .sized_body(user.len(), Cursor::new(user))
-            .raw_header("X-USER-ID", self.uuid.to_string())
-            .merge(base_response)
-            .ok()
-    }
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("We cannot find this page {}.", req.uri())
 }
 
-struct NewUser<'a>(Vec<&'a User>);
-
-fn default_response<'r>() -> response::Responder<'r> {
-    Respose::build()
-        .header(ContentType::Plain)
-        .raw_header("X-CUSTOM-ID", "CUSTOM")
-        .finalized()
+#[catch(403)]
+fn forbidden(req: &Request) -> String {
+    format!("Access forbidden {}.", req.uri())
 }
 
 #[get("/user/<uuid>", rank = 1, format = "text/plain")]
 fn user(uuid: &str) -> Option<&User> {
-    let user = USERS.get(uuid);
-    match user {
-        Some(u) => Some(u),
-        None => None,
-    }
+    USERS.get(uuid)
 }
 
 #[get("/users/<name_grade>?<filters..>")]
-fn users(name_grade: NameGrade, filters: Option<Filters>) -> Option<NewUser> {
+fn users(name_grade: NameGrade, filters: Option<Filters>) -> Result<NewUser, Status> {
     let users: Vec<&User> = USERS
         .values()
-        .filter(|user|
-            user.name.contains(&name_grade.name)
-                &&
-                user.grade == name_grade.grade
-        )
+        .filter(|user| user.name.contains(&name_grade.name) && user.grade == name_grade.grade)
         .filter(|user| {
             if let Some(fts) = &filters {
                 user.age == fts.age && user.active == fts.active
@@ -113,16 +133,15 @@ fn users(name_grade: NameGrade, filters: Option<Filters>) -> Option<NewUser> {
             }
         })
         .collect();
-
-    if users.len() > 0 {
-        Some(NewUser(users))
+    if users.is_empty() {
+        Err(Status::Forbidden)
     } else {
-        None
+        Ok(NewUser(users))
     }
 }
-
 
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build().mount("/", routes![user, users])
+        .register("/", catchers![not_found, forbidden])
 }
